db39fd8e8b048f8dc07e068f7876b48a9847ad64
AshleyXM <454198339@qq.com>
2024-06-27 23:22:52 -0700

    Implement Publish component

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,5 +1,80 @@
+import { Card, Breadcrumb, Form, Button, Input, Space, Select } from "antd";
+import ReactQuill from "react-quill";
+import { Link } from "react-router-dom";
+import "react-quill/dist/quill.snow.css";
+import "./index.scss";
+
+const { Option } = Select;
+
 const Publish = () => {
-  return <div>this is Publish</div>;
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>Home</Link> },
+              { title: "Post a New Article" },
+            ]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[
+              { required: true, message: "Please enter your article title" },
+            ]}
+          >
+            <Input
+              placeholder="Please enter your article title"
+              style={{ width: 400 }}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Channel"
+            name="channel_id"
+            rules={[
+              { required: true, message: "Please select your article channel" },
+            ]}
+          >
+            <Select
+              placeholder="Please select your article channel"
+              style={{ width: 400 }}
+            >
+              <Option value={0}>Art</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Content"
+            name="content"
+            rules={[
+              { required: true, message: "Please enter your article content" },
+            ]}
+          >
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="Please enter your article content"
+            />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                Publish
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Publish;
