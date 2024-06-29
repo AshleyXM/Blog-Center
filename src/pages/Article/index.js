d914124eb412ecefc04a90197ba98cd7abfdaf0c
AshleyXM <454198339@qq.com>
2024-06-28 17:00:36 -0700

    Get article list by calling api

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -13,81 +13,88 @@ import { Table, Tag, Space } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import img404 from "@/assets/error.png";
 import { useChannel } from "@/hooks/useChannel";
+import { useEffect, useState } from "react";
+import { getArticleListAPI } from "@/apis/article";
 
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 
+const status = {
+  0: <Tag color="error">Draft</Tag>,
+  1: <Tag color="warning">Being reviewed</Tag>,
+  2: <Tag color="success">Published</Tag>,
+};
+
+const columns = [
+  {
+    title: "Cover",
+    dataIndex: "cover",
+    width: 120,
+    render: (cover) => {
+      return (
+        <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+      );
+    },
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+    width: 180,
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    render: (type) => status[type],
+  },
+  {
+    title: "Publication Date",
+    dataIndex: "pub_date",
+  },
+  {
+    title: "Count of Views",
+    dataIndex: "view_count",
+  },
+  {
+    title: "Count of Comments",
+    dataIndex: "comment_count",
+  },
+  {
+    title: "Count of Likes",
+    dataIndex: "like_count",
+  },
+  {
+    title: "Action",
+    render: (data) => {
+      return (
+        <Space size="middle">
+          <Button type="primary" shape="circle" icon={<EditOutlined />} />
+          <Button
+            type="primary"
+            danger
+            shape="circle"
+            icon={<DeleteOutlined />}
+          />
+        </Space>
+      );
+    },
+  },
+];
+
 const Article = () => {
   const { channelList } = useChannel();
-  const columns = [
-    {
-      title: "Cover",
-      dataIndex: "cover",
-      width: 120,
-      render: (cover) => {
-        return (
-          <img src={cover.images[0] || img404} width={80} height={60} alt="" />
-        );
-      },
-    },
-    {
-      title: "Title",
-      dataIndex: "title",
-      width: 220,
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      render: (data) => <Tag color="green">Published</Tag>,
-    },
-    {
-      title: "Publication Date",
-      dataIndex: "pubdate",
-    },
-    {
-      title: "Count of Views",
-      dataIndex: "read_count",
-    },
-    {
-      title: "Count of Comments",
-      dataIndex: "comment_count",
-    },
-    {
-      title: "Count of Likes",
-      dataIndex: "like_count",
-    },
-    {
-      title: "Action",
-      render: (data) => {
-        return (
-          <Space size="middle">
-            <Button type="primary" shape="circle" icon={<EditOutlined />} />
-            <Button
-              type="primary"
-              danger
-              shape="circle"
-              icon={<DeleteOutlined />}
-            />
-          </Space>
-        );
-      },
-    },
-  ];
 
-  const data = [
-    {
-      id: "8218",
-      comment_count: 0,
-      cover: {
-        images: [],
-      },
-      like_count: 0,
-      pubdate: "2019-03-11 09:00:00",
-      read_count: 2,
-      status: 2,
-      title: "wkwebview offline loading h5 sesource solution",
-    },
-  ];
+  const [articleList, setArticleList] = useState([]);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    async function getArticleList() {
+      const res = await getArticleListAPI();
+      setArticleList(res.data.results);
+      setCount(res.data.total);
+    }
+
+    getArticleList();
+  }, []);
+
   return (
     <div>
       <Card
@@ -131,8 +138,8 @@ const Article = () => {
           </Form.Item>
         </Form>
       </Card>
-      <Card title={`{count} Results in Total Found per the Filter Condition:`}>
-        <Table rowKey="id" columns={columns} dataSource={data} />
+      <Card title={`${count} Results in Total Found per the Filter Condition:`}>
+        <Table rowKey="id" columns={columns} dataSource={articleList} />
       </Card>
     </div>
   );
